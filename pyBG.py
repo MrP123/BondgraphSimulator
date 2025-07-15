9798351d3de8fe726e1690ca87d992635e038cbb
Matthias Panny <matthias.panny@mci.edu>
2025-07-15 15:03:14 +0200

    feat: Significantly improved generation of state space matrices.

--- pyBG.py
+++ pyBG.py
@@ -39,7 +39,7 @@ class Bond:
 class ElementOnePort(Node, ABC):
     def __init__(self, name: str, value: str):
         super().__init__(name)
-        self.value = sp.Symbol(value)
+        self.value = sp.Symbol(value, real=True, positive=True)  # Ensure value is a positive real number
         self.bond: Bond = None # bond that connects this element to a bond graph
     
     @property
@@ -58,6 +58,15 @@ class SourceEffort(ElementOnePort):
     def equations(self) -> list[sp.Expr]:
         # Source --> constant effort
         return [self.bond.effort - self.value]
+
+class SourceFlow(ElementOnePort):
+    def __init__(self, name: str, value: str):
+        super().__init__(name, value)
+
+    @property
+    def equations(self) -> list[sp.Expr]:
+        # Source --> constant flow
+        return [self.bond.flow - self.value]
 
 class Capacitor(ElementOnePort, StatefulElement):
     def __init__(self, name: str, value: str):
@@ -100,7 +109,7 @@ class Resistor(ElementOnePort):
 class ElementTwoPort(Node, ABC):
     def __init__(self, name: str, value: str):
         super().__init__(name)
-        self.value = sp.Symbol(value)
+        self.value = sp.Symbol(value, real=True, positive=True)
         self.bond1: Bond = None # ElementOther   --(bond1)--> ElementTwoPort
         self.bond2: Bond = None # ElementTwoPort --(bond2)--> ElementOther
     
@@ -127,7 +136,24 @@ class Transformer(ElementTwoPort):
         if self.bond1.causality == "effort_out":
             return [self.bond1.flow - 1/self.value * self.bond2.flow, self.bond2.effort - 1 / self.value * self.bond1.effort]
         elif self.bond1.causality == "flow_out":
-            return [self.bond1.effort - self.value * self.bond2.effort, self.bond2.flow - self.value * self.bond1.flow]        
+            return [self.bond1.effort - self.value * self.bond2.effort, self.bond2.flow - self.value * self.bond1.flow]
+        
+class Gyrator(ElementTwoPort):
+    def __init__(self, name: str, value: str):
+        super().__init__(name, value)
+
+    @property
+    def equations(self) -> list[sp.Expr]:
+        if self.bond1 is None or self.bond2 is None:
+            raise ValueError("Both bonds must be assigned to the gyrator element.")
+
+        if self.bond1.causality == self.bond2.causality:
+            raise ValueError("Both bonds must have the different causality for a gyrator element.")
+
+        if self.bond1.causality == "effort_out":
+            return [self.bond1.flow - 1/self.value * self.bond2.effort, self.bond2.flow - 1 / self.value * self.bond1.effort]
+        elif self.bond1.causality == "flow_out":
+            return [self.bond1.effort - self.value * self.bond2.flow, self.bond2.effort - self.value * self.bond1.flow]
         
 
 class Junction(Node, ABC):
@@ -177,47 +203,51 @@ class BondGraph():
 
         self.state_vars: list[sp.Expr] = []
         self.equations: list[sp.Expr] = []
+        self.inputs: list[sp.Symbol] = []
 
     def add_element(self, element: Node):
         self.elements.append(element)
+
+        if isinstance(element, SourceEffort) or isinstance(element, SourceFlow):
+            self.inputs.append(element.value)
 
     def add_connection(self, bond: Bond):
         self.connections.append(bond)
 
     def handle_bonds(self):
-        for bond in self.connections:
-            
-            def handle_bond_element(element: Node, bond: Bond):
-                if isinstance(element, ElementOnePort):
-                    element.bond = bond #Assign bond to element
+        
+        def handle_bond_element(element: Node, bond: Bond):
+            if isinstance(element, ElementOnePort):
+                element.bond = bond
                 
-                elif isinstance(element, ElementTwoPort):
-                    if bond.to_element == element:
-                        element.bond1 = bond
-                    elif bond.from_element == element:
-                        element.bond2 = bond
+            elif isinstance(element, ElementTwoPort):
+                if bond.to_element == element:
+                    element.bond1 = bond
+                elif bond.from_element == element:
+                    element.bond2 = bond
 
-                elif isinstance(element, Junction):
-                    element.bonds.append(bond)
+            elif isinstance(element, Junction):
+                element.bonds.append(bond)
 
-                    if isinstance(element, OneJunction):
-                        if (bond.from_element == element and bond.causality == "effort_out") or (bond.to_element == element and bond.causality == "flow_out"):
-                            # bond is strong bond for zero junction
-                            if element.strong_bond is None:
-                                element.strong_bond = bond
-                                print(f"Assigned strong bond {bond} to OneJunction {element}.")
-                            else:
-                                raise ValueError(f"OneJunction {element} already has a strong bond: {element.strong_bond}. Cannot assign {bond}.")
+                if isinstance(element, OneJunction):
+                    if (bond.from_element == element and bond.causality == "effort_out") or (bond.to_element == element and bond.causality == "flow_out"):
+                        # bond is strong bond for zero junction
+                        if element.strong_bond is None:
+                            element.strong_bond = bond
+                            print(f"Assigned strong bond {bond} to OneJunction {element}.")
+                        else:
+                            raise ValueError(f"OneJunction {element} already has a strong bond: {element.strong_bond}. Cannot assign {bond}.")
 
-                    elif isinstance(element, ZeroJunction):
-                        if (bond.from_element == element and bond.causality == "flow_out") or (bond.to_element == element and bond.causality == "effort_out"):
-                            # bond is strong bond for zero junction
-                            if element.strong_bond is None:
-                                element.strong_bond = bond
-                                print(f"Assigned strong bond {bond} to OneJunction {element}.")
-                            else:
-                                raise ValueError(f"ZeroJunction {element} already has a strong bond: {element.strong_bond}. Cannot assign {bond}.")
-
+                elif isinstance(element, ZeroJunction):
+                    if (bond.from_element == element and bond.causality == "flow_out") or (bond.to_element == element and bond.causality == "effort_out"):
+                        # bond is strong bond for zero junction
+                        if element.strong_bond is None:
+                            element.strong_bond = bond
+                            print(f"Assigned strong bond {bond} to ZeroJunction {element}.")
+                        else:
+                            raise ValueError(f"ZeroJunction {element} already has a strong bond: {element.strong_bond}. Cannot assign {bond}.")
+        
+        for bond in self.connections:
             handle_bond_element(bond.from_element, bond)
             handle_bond_element(bond.to_element, bond)
 
@@ -247,7 +277,7 @@ class BondGraph():
             elif isinstance(element, Junction):
                 bond_graph.equations.extend(element.equations)
 
-    def get_state_equations(self):
+    def get_solution_equations(self):
         state_derivatives = [sp.Derivative(var, 't') for var in self.state_vars]
 
         solution = sp.solve(self.equations, state_derivatives + [b.effort for b in self.connections] + [b.flow for b in self.connections])
@@ -256,17 +286,14 @@ class BondGraph():
 bond_graph = BondGraph()
 
 #https://en.wikipedia.org/wiki/Bond_graph#:~:text=denotes%20preferred%20causality.-,For%20the%20example%20provided,-%2C
-voltage_source = SourceEffort("V", "V(t)")  # Spannungsquelle
-capacitor = Capacitor("C6", "C6")             # Kapazität
-inductor = Inductor("I3", "L3")               # Induktivität
-
-resistor2 = Resistor("R2", "R2")               # Widerstand
-resistor7 = Resistor("R7", "R7")               # Widerstand
-
-transformer = Transformer("T1", "T1")           # Transformator
-
-junction1 = OneJunction("J1")                 # 1-Junction
-junction0 = ZeroJunction("J0")                 # 0-Junction
+voltage_source = SourceEffort("V", "V(t)")
+capacitor = Capacitor("C6", "C6")
+inductor = Inductor("I3", "L3")
+resistor2 = Resistor("R2", "R2")
+resistor7 = Resistor("R7", "R7")
+transformer = Transformer("T1", "T1")
+junction1 = OneJunction("J1")
+junction0 = ZeroJunction("J0")
 
 bond_graph.add_element(voltage_source)
 bond_graph.add_element(capacitor)
@@ -299,77 +326,37 @@ bond_graph.add_connection(Bond(junction0, resistor7, "effort_out"))
 
 bond_graph.handle_bonds()
 bond_graph.handle_equations()
+solution = bond_graph.get_solution_equations()
 
-solution = bond_graph.get_state_equations()
-
-# Determine A, B, C, D matrices for SS representation
 n_states = len(bond_graph.state_vars)
-n_inputs = 1
-n_outputs = 1
+n_inputs = len(bond_graph.inputs)  # Number of inputs (sources)
+n_outputs = 2*len(bond_graph.connections) #effort & flow for each bond
 
-A = sp.zeros(n_states, n_states)
-B = sp.zeros(n_states, n_inputs)
+# General form of a state space model
+# x_dot = f(x, u)
+#     y = h(x, u)
+# Simplification for linear systems:
+# x_dot = A*x + B*u
+#     y = C*x + D*u
+# --> therefore
+# A = ∂f/∂x, B = ∂f/∂u, C = ∂h/∂x, D = ∂h/∂u each at stationary point 0
 
-# Extract the state derivatives (dx/dt) from the solution
-for i, var in enumerate(bond_graph.state_vars):
-    derivative = sp.Derivative(var, 't')  # symbolic derivatives
+f: sp.Matrix = sp.zeros(n_states, 1)
+for i, state_var in enumerate(bond_graph.state_vars):
+    state_deriv = sp.Derivative(state_var, 't')  # symbolic derivative dx/dt
+    f[i] = solution[state_deriv]
 
-    if derivative in solution:  # check if they are in the solution
-        expr: sp.Expr = solution[derivative]  # expression for dx/dt
-        for j, state_var in enumerate(bond_graph.state_vars):
-            A[i, j] = expr.diff(state_var)  # partial derivative w.r.t. state variables as A matrix links dx/dt with x via dx/dt ~ A*x
+h: sp.Matrix = sp.zeros(n_outputs, 1) # efforts then flows
+for i, bond in enumerate(bond_graph.connections):
+    h[i] = solution[bond.effort]
+    h[i + n_outputs//2] = solution[bond.flow]
 
-        input_counter = 0
-        for element in bond_graph.elements:
+A = f.jacobian(bond_graph.state_vars)
+B = f.jacobian(bond_graph.inputs)
 
-            # Only check source elements for inputs
-            if isinstance(element, SourceEffort):
-                if element.bond.effort in solution:
-                    input_var = solution[element.bond.effort]
-
-                    B[i, input_counter] = expr.diff(input_var) # partial derivatives of dx/dt w.r.t. inputs as dx/dt ~ B*u
-                    input_counter += 1
-            
-            #input_var = solution[efforts["V"]]
-            #B[i, 0] = expr.diff(input_var)  
-
-for element in bond_graph.elements:
-    # Only check non-source elements
-    if not isinstance(element, SourceEffort) and not isinstance(element, Junction):
-
-        # Get output matrices C for effort and flow in element
-        if element.bond.effort in solution:
-            C_effort = sp.zeros(n_outputs, n_states)
-            for j, state_var in enumerate(bond_graph.state_vars):
-                C_effort[0, j] = solution[element.bond.effort].diff(state_var)
-                
-        if element.bond.flow in solution:
-            C_flow = sp.zeros(n_outputs, n_states)
-            for j, state_var in enumerate(bond_graph.state_vars):
-                C_flow[0, j] = solution[element.bond.flow].diff(state_var)
-
-        # Assign to specific variables based on element type
-        if isinstance(element, Capacitor):
-            C_voltage_C = C_effort
-            C_current_C = C_flow
-        elif isinstance(element, Inductor):
-            C_voltage_I = C_effort
-            C_current_I = C_flow
-        elif isinstance(element, Resistor):
-            C_voltage_R = C_effort
-            C_current_R = C_flow
-
-# All variants of the C matrix
-C_mats = {
-    "voltage_C": C_voltage_C,
-    "current_C": C_current_C,
-    "voltage_I": C_voltage_I,
-    "current_I": C_current_I,
-    "voltage_R": C_voltage_R,
-    "current_R": C_current_R,
-}
-
-D = sp.zeros(1, 1)
+C = h.jacobian(bond_graph.state_vars)
+D = h.jacobian(bond_graph.inputs)
+# alternatively could use sp.linear_eq_to_matrix(...)
 
 # Print results
 print("Matrix A:")
@@ -377,8 +364,81 @@ sp.pprint(A)
 print("\nMatrix B:")
 sp.pprint(B)
 print("\nMatrix C:")
-for key, C_l in C_mats.items():
-    print(f"\n{key}:")
-    sp.pprint(C_l)
+sp.pprint(C)
 print("\nMatrix D:")
 sp.pprint(D)
+
+# Initial conditions for the state variables --> not yet working
+#x0_entries = sp.symbols(f"x0_0:{n_states}", real=True)
+#y0_entries = sp.symbols(f"y0_0:{n_outputs}", real=True)
+#x0 = sp.Matrix(x0_entries)
+#y0 = sp.Matrix(y0_entries)
+#U0_C6 = sp.symbols('U0_C6', real=True)
+#U0_I3 = sp.symbols('U0_I3', real=True)
+
+#free_params = sp.symbols('y0_0:2 y0_3:5 y0_6:14', real=True)
+#y0_free = sp.Matrix([*free_params[:2], U0_I3, *free_params[2:5], U0_C6, *free_params[5:]])
+
+#for i, bond in enumerate(bond_graph.connections):
+#    if bond.effort == capacitor.bond.effort:
+#        print(f"capacitor = {i}")
+#    if bond.flow == inductor.bond.flow:
+#        print(f"inductor = {i}")
+
+#equation = C * x0 - y0_free
+
+#scalar_equations = [equation[i, 0] for i in range(n_outputs)]
+
+# Solve the system for x0
+#solutions = sp.linsolve(scalar_equations, x0)
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctrl
+
+R_val = 470.0
+C_val = 10e-6
+L_val = 1e-3
+V_val = 1.0
+T_val = 2.0
+
+#ToDo: maybe make numeric value part of the element class?
+subs_dict = {
+    resistor2.value:      R_val,
+    resistor7.value:      R_val,
+    capacitor.value:      C_val,
+    inductor.value:       L_val,
+    transformer.value:    T_val,
+    voltage_source.value: V_val
+}
+
+A_mat_val = np.array(A.subs(subs_dict), dtype=np.float64)
+B_mat_val = np.array(B.subs(subs_dict), dtype=np.float64)
+C_mat_val = np.array(C.subs(subs_dict), dtype=np.float64)
+D_mat_val = np.array(D.subs(subs_dict), dtype=np.float64)
+
+x0_val = np.zeros_like(B_mat_val)
+
+sys = ctrl.ss(A_mat_val, B_mat_val, C_mat_val, D_mat_val)
+T, yout = ctrl.step_response(sys, T=25.0e-3, X0=x0_val)
+yout = np.squeeze(yout) # yout is n_outputs x 1 x n_timesteps as the C matrix is has a shape of (n_outputs, n_states), so we need to squeeze the output to n_outputs x n_timesteps
+
+print(f"Max time step: {np.max(np.diff(T))}")
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+ax1.set_xlabel("time (s)")
+ax1.set_ylabel("Efforts (V, N, Pa)")
+ax2.set_ylabel("Currents (A, m/s, m^3/s)")
+
+for i, signal in enumerate(yout):
+    if i < n_outputs // 2:
+        ax1.plot(T, signal, label=f"e_{i}")
+    else:
+        ax2.plot(T, signal, label=f"f_{i - n_outputs // 2}")
+    
+ax1.legend()
+ax2.legend()
+plt.show()
